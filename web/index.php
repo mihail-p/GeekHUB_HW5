e24da4e129ee2c2b2e97f51c3ff7711865c0bfa9
Mihail Pronyuk <mishaprk@gmail.com>
2015-10-29 19:57:27 +0200

    edit index.php

--- web/index.php
+++ web/index.php
@@ -2,4 +2,21 @@
 
 require __DIR__ . '/../config/autoload.php';
 
-echo "Hi ALL!";
+echo "Hi ALL__!";
+
+print_r('<br /> show databases:<br />');
+
+$user='root'; $pass='gfhjkmlkzsql';
+try {
+    $dbh = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+    foreach($dbh->query('show databases') as $row) {
+
+        print_r($row);
+        echo('<br />');
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
