f8d894c6b8e06dcc65b9512de5aa6baaf758d393
Mihail Pronyuk <mishaprk@gmail.com>
2015-11-03 23:18:58 +0200

    fix "find" module

--- web/view/showWord.php
+++ web/view/showWord.php
@@ -1,44 +1,49 @@
-<p>
-    <b>Find <i>eng_word</i>:</b>
-</p>
-<table border='1'>
-    <tr id="table_header">
-        <td>eng word</td>
-        <td>ua/ru word</td>
-    </tr>
-    <?php
-    //var_dump($_POST['response']);
-    //var_dump($response);
-
-    $arr = $response;
-    print "<tr>";
-    $eng_word = '';
-    $id_ua = 0;
-    $id = 0;
-    foreach ($arr as $key => $value) {
-        foreach ($arr[$key] as $keyfield => $valuefield) {
-            if ($keyfield == 'id_en') {
-            }
-            if ($keyfield == 'eng_word') {
-                $eng_word = $valuefield;
-                print "<td><b>" . $valuefield . " </b></td>";
-            }
-            if ($keyfield == 'id_ua') {
-                $id_ua = $valuefield;
-            }
-            if ($keyfield == 'id_1') {
-                $id = $valuefield;
-            }
-            if ($keyfield == 'ua_word') {
-                print "<td>" . $valuefield . " </td>";
-            }
-        };
-        print "</tr>";
-    }
+<?php
+if (count($response)>=1) {
     ?>
-</table>
-Add one more translation
-<form method="POST" action="./view/addOne.php">
-    <input type="hidden" name="id_1" value="<?php echo $id; ?>">
-    <input type="submit" value="add">
-</form>
+    <p>
+        <b>Find <i>eng_word</i>:</b>
+    </p>
+
+    <table border='1'>
+        <tr id="table_header">
+            <td>eng word</td>
+            <td>ua/ru word</td>
+        </tr>
+        <?php
+        $arr = $response;
+        print "<tr>";
+        $eng_word = '';
+        $id_ua = 0;
+        $id = 0;
+        foreach ($arr as $key => $value) {
+            foreach ($arr[$key] as $keyfield => $valuefield) {
+                if ($keyfield == 'id_en') {
+                }
+                if ($keyfield == 'eng_word') {
+                    $eng_word = $valuefield;
+                    print "<td><b>" . $valuefield . " </b></td>";
+                }
+                if ($keyfield == 'id_ua') {
+                    $id_ua = $valuefield;
+                }
+                if ($keyfield == 'id_1') {
+                    $id = $valuefield;
+                }
+                if ($keyfield == 'ua_word') {
+                    print "<td>" . $valuefield . " </td>";
+                }
+            };
+            print "</tr>";
+        }
+        ?>
+    </table>
+    Add one more translation
+    <form method="POST" action="./view/addOne.php">
+        <input type="hidden" name="id_1" value="<?php echo $id; ?>">
+        <input type="submit" value="add">
+    </form>
+    <?php
+} else {
+    echo '<p>word not found</p>';
+}
